15a6a77be85ade4491923596d367ab3cb2192787
frisyk <frisnadi1@gmail.com>
2024-11-21 15:15:30 +0700

    refactor: add ci/cd test image

--- src/components/AddThreadButton.jsx
+++ src/components/AddThreadButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from './ui/Buttoncomp';
+import Button from './ui/Buttoncomp.tsx';
 
-const AddThreadButton = () => {
+function AddThreadButton() {
   const navigate = useNavigate();
 
   const onAddButtonClicked = () => navigate('/add-thread');
@@ -14,6 +14,6 @@ const AddThreadButton = () => {
       className="fixed m-5 right-0 bottom-0 px-8 py-4 bg-blue-800 text-blue-50 rounded-xl hover:bg-blue-600"
     />
   );
-};
+}
 
 export default AddThreadButton;
